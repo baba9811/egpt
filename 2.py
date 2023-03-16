78a365e4f8b51c59740f52736efeb4fbc85c3199
Kyubum Hwang <bany1111@Kyubums-MacBook-Pro.local>
2023-03-17 02:53:38 +0900

    03_17

--- 2.py
+++ 2.py
@@ -1,13 +1,106 @@
 import pandas as pd
-from transformers import AutoTokenizer, AutoModelWithLMHead
+from transformers import GPT2LMHeadModel, AutoTokenizer
 import torch
 
 # 데이터셋 로드
 data = pd.read_csv("data.csv", encoding='cp949')
 
 # 토크나이저 및 모델 로드
-tokenizer = AutoTokenizer.from_pretrained("skt/kogpt2-base-v2", eos_token='</s>')
-model = AutoModelWithLMHead.from_pretrained("skt/kogpt2-base-v2")
+tokenizer = AutoTokenizer.from_pretrained("skt/kogpt2-base-v2")
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
+
+# 데이터셋 가공
+input_ids = []
+attention_masks = []
+labels = []
+for idx, row in data.iterrows():
+    context = str(row["context"])
+    response = str(row["response"])
+    input_text = "[CLS]" + context + "[SEP]" + response + "[SEP]"
+    input_tokenized = tokenizer.encode_plus(input_text, max_length=1024, padding='longest', return_tensors='pt')
+    input_ids.append(input_tokenized['input_ids'])
+    attention_masks.append(input_tokenized['attention_mask'])
+    labels.append(torch.tensor(int(row["score"])))
+
+input_ids = torch.cat(input_ids, dim=0)
+attention_masks = torch.cat(attention_masks, dim=0)
+labels = torch.stack(labels)
+
+# 모델 학습
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+for epoch in range(5):
+    model.train()
+    optimizer.zero_grad()
+
+    outputs = model(input_ids, attention_mask=attention_masks, labels=input_ids)
+    loss = loss_fn(outputs.logits.view(-1, outputs.logits.shape[-1]), input_ids.view(-1))
+
+    loss.backward()
+    optimizer.step()
+
+# 파인튜닝된 모델 저장
+model.save_pretrained("chatbot_model")
+tokenizer.save_pretrained("chatbot_model")
+import pandas as pd
+from transformers import GPT2LMHeadModel, AutoTokenizer
+import torch
+
+# 데이터셋 로드
+data = pd.read_csv("data.csv", encoding='cp949')
+
+# 토크나이저 및 모델 로드
+tokenizer = AutoTokenizer.from_pretrained("skt/kogpt2-base-v2")
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
+
+# 데이터셋 가공
+input_ids = []
+attention_masks = []
+labels = []
+for idx, row in data.iterrows():
+    context = str(row["context"])
+    response = str(row["response"])
+    input_text = "[CLS]" + context + "[SEP]" + response + "[SEP]"
+    input_tokenized = tokenizer.encode_plus(input_text, max_length=1024, padding='longest', return_tensors='pt')
+    input_ids.append(input_tokenized['input_ids'])
+    attention_masks.append(input_tokenized['attention_mask'])
+    labels.append(torch.tensor(int(row["score"])))
+
+input_ids = torch.cat(input_ids, dim=0)
+attention_masks = torch.cat(attention_masks, dim=0)
+labels = torch.stack(labels)
+
+# 모델 학습
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+for epoch in range(5):
+    model.train()
+    optimizer.zero_grad()
+
+    outputs = model(input_ids, attention_mask=attention_masks, labels=input_ids)
+    loss = loss_fn(outputs.logits.view(-1, outputs.logits.shape[-1]), input_ids.view(-1))
+
+    loss.backward()
+    optimizer.step()
+
+# 파인튜닝된 모델 저장
+model.save_pretrained("chatbot_model")
+tokenizer.save_pretrained("chatbot_model")
+import pandas as pd
+from transformers import GPT2LMHeadModel, AutoTokenizer
+import torch
+
+# 데이터셋 로드
+data = pd.read_csv("data.csv", encoding='cp949')
+
+# 토크나이저 및 모델 로드
+tokenizer = AutoTokenizer.from_pretrained("skt/kogpt2-base-v2")
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
 
 # 데이터셋 가공
 input_ids = []
