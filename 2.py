5db02a2860e89706b42773642f01be9df64c1b6d
Kyubum Hwang <bany1111@Kyubums-MacBook-Pro.local>
2023-03-17 02:52:28 +0900

    03_17

--- 2.py
+++ 2.py
@@ -1,23 +1,13 @@
 import pandas as pd
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, PreTrainedTokenizerFast
+from transformers import AutoTokenizer, AutoModelWithLMHead
 import torch
 
 # 데이터셋 로드
 data = pd.read_csv("data.csv", encoding='cp949')
 
 # 토크나이저 및 모델 로드
-tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2", eos_token='</s>')
-tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-
-max_length = 1024
-if max_length > tokenizer.max_len_single_sentence:
-    max_length = tokenizer.max_len_single_sentence
-if max_length > tokenizer.model_max_length:
-    max_length = tokenizer.model_max_length
-tokenizer.model_max_length = max_length
-
-model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
-
+tokenizer = AutoTokenizer.from_pretrained("skt/kogpt2-base-v2", eos_token='</s>')
+model = AutoModelWithLMHead.from_pretrained("skt/kogpt2-base-v2")
 
 # 데이터셋 가공
 input_ids = []
@@ -27,12 +17,11 @@ for idx, row in data.iterrows():
     context = str(row["context"])
     response = str(row["response"])
     input_text = "[CLS]" + context + "[SEP]" + response + "[SEP]"
-    input_tokenized = tokenizer.encode_plus(input_text, max_length=1024, padding='max_length', return_tensors='pt')
+    input_tokenized = tokenizer.encode_plus(input_text, max_length=1024, padding='longest', return_tensors='pt')
     input_ids.append(input_tokenized['input_ids'])
     attention_masks.append(input_tokenized['attention_mask'])
     labels.append(torch.tensor(int(row["score"])))
 
-input_tokenized = tokenizer.encode_plus(input_text, max_length=1024, padding='max_length', return_tensors='pt')
 input_ids = torch.cat(input_ids, dim=0)
 attention_masks = torch.cat(attention_masks, dim=0)
 labels = torch.stack(labels)
