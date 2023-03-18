7c0f21b20ae66242868057b130bb7128a1994bfc
Kyubum Hwang <bany1111@Kyubums-MacBook-Pro.local>
2023-03-18 18:33:01 +0900

    03_18

--- train_kogpt2.py
+++ train_kogpt2.py
@@ -2,6 +2,7 @@ import pandas as pd
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast, GPT2Config, GPT2LMHeadModel
 from transformers import TextDataset, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
+from transformers import DataCollatorForSeq2Seq
 
 # 데이터셋 불러오기
 data = pd.read_csv('data.csv', encoding='cp949')
@@ -12,25 +13,31 @@ model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
 
 # 데이터셋 전처리
 def prepare_dataset(data):
-    dialogues = []
+    input_texts = []
+    output_texts = []
     for i, row in data.iterrows():
         context = row['context']
         response = row['response']
-        score = row['score']
-        dialogue = f"{context}  {response}"
-        dialogues.append(dialogue)
-    return dialogues
+        input_texts.append(context)
+        output_texts.append(response)
+    return input_texts, output_texts
 
 # train.txt 파일 생성
-train_dialogues = prepare_dataset(data)
-with open("train.txt", "w", encoding="utf-8") as f:
-    for dialogue in train_dialogues:
-        f.write(dialogue + "\n")
+train_input_texts, train_output_texts = prepare_dataset(data)
+with open("train_input.txt", "w", encoding="utf-8") as f1, open("train_output.txt", "w", encoding="utf-8") as f2:
+    for input_text, output_text in zip(train_input_texts, train_output_texts):
+        f1.write(input_text + "\n")
+        f2.write(output_text + "\n")
 
 # 학습 데이터셋 준비
 train_data = TextDataset(
     tokenizer=tokenizer,
-    file_path="train.txt",
+    file_path="train_input.txt",
+    block_size=128,
+)
+train_labels = TextDataset(
+    tokenizer=tokenizer,
+    file_path="train_output.txt",
     block_size=128,
 )
 
@@ -48,15 +55,16 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    data_collator=DataCollatorForLanguageModeling(
-        tokenizer=tokenizer, mlm=False,
+    data_collator=DataCollatorForSeq2Seq(
+        tokenizer=tokenizer,
+        model=model,
     ),
     train_dataset=train_data,
+    train_labels=train_labels,
 )
 
 trainer.train()
 
- # 토크나이저와 모델 저장
-
+# 토크나이저와 모델 저장
 tokenizer.save_pretrained("./kogpt2_fine_tuned")
 model.save_pretrained("./kogpt2_fine_tuned")
