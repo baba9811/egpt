8acee6029225988f927a60aba9d11e64d071ce3f
Kyubum Hwang <bany1111@Kyubums-MacBook-Pro.local>
2023-03-18 18:36:59 +0900

    03_18

--- train_kogpt2.py
+++ train_kogpt2.py
@@ -1,45 +1,38 @@
 import pandas as pd
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast, GPT2Config, GPT2LMHeadModel
-from transformers import TextDataset, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForSeq2Seq
+from transformers import T2TModel
+from datasets import Dataset
 
 # 데이터셋 불러오기
 data = pd.read_csv('data.csv', encoding='cp949')
 
 # KoGPT2 토크나이저 및 모델 불러오기
 tokenizer = GPT2TokenizerFast.from_pretrained("skt/kogpt2-base-v2")
-model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
+config = GPT2Config.from_pretrained("skt/kogpt2-base-v2")
+model = T2TModel.from_pretrained("skt/kogpt2-base-v2", config=config)
 
 # 데이터셋 전처리
 def prepare_dataset(data):
     input_texts = []
     output_texts = []
     for i, row in data.iterrows():
-        context = str(row['context'])
-        response = str(row['response'])
+        context = row['context']
+        response = row['response']
         input_texts.append(context)
         output_texts.append(response)
     return input_texts, output_texts
 
 # train.txt 파일 생성
 train_input_texts, train_output_texts = prepare_dataset(data)
-with open("train_input.txt", "w", encoding="utf-8") as f1, open("train_output.txt", "w", encoding="utf-8") as f2:
-    for input_text, output_text in zip(train_input_texts, train_output_texts):
-        f1.write(input_text + "\n")
-        f2.write(output_text + "\n")
 
 # 학습 데이터셋 준비
-train_data = TextDataset(
-    tokenizer=tokenizer,
-    file_path="train_input.txt",
-    block_size=128,
-)
-train_labels = TextDataset(
-    tokenizer=tokenizer,
-    file_path="train_output.txt",
-    block_size=128,
-)
+train_dataset = Dataset.from_dict({
+    'input_text': train_input_texts,
+    'output_text': train_output_texts,
+})
+train_dataset = train_dataset.map(lambda x: tokenizer(x['input_text'], x['output_text'], padding='max_length', truncation=True, max_length=128), batched=True)
 
 # 학습 설정
 training_args = TrainingArguments(
@@ -59,8 +52,7 @@ trainer = Trainer(
         tokenizer=tokenizer,
         model=model,
     ),
-    train_dataset=train_data,
-    train_labels=train_labels,
+    train_dataset=train_dataset,
 )
 
 trainer.train()
